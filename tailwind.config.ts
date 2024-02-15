6d59ca7ff96eee5b267569ca5a7e7a394100758a
asteof <kirill.brezhnev02@gmail.com>
2024-02-15 22:20:48 +0200

    created articles and advices views

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -47,6 +47,26 @@ export default <Partial<Config>>{
           950: '#121212',
         },
       },
+      keyframes: {
+        'slide-off': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(1000px)' },
+        },
+        'horizontal-bounce': {
+          '0%, 100%': {
+            transform: 'translateX(-25%)',
+            'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateX(0)',
+            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
+      },
+      animation: {
+        'slide-off': 'slide-off 0.5s cubic-bezier(0,.01,1,.21) 1 forwards',
+        'horizontal-bounce': 'horizontal-bounce 1s infinite',
+      },
     },
   },
 };
