283fb1565f190b3410d9ea2fc245351d468c492a
Kyrylo <kirill.brezhnev02@gmail.com>
2024-03-02 00:50:08 +0200

    implemented utils and utility types for new categories

---
+++ utils/SerializeNonPOJOs.ts
@@ -0,0 +1,17 @@
+import type { CategoryTemplateCollection } from '~/utils/types/CategoryTypes';
+
+const serializeNonPOJOs = (value: object | null) => {
+  return structuredClone(value);
+};
+
+const serializeNonPOJOsArray = (nonPOJOs: Array<object | null>) => {
+  return nonPOJOs.map((value) => serializeNonPOJOs(value));
+};
+
+const mapTemplateToSerializedCategory = (template: CategoryTemplateCollection) => {
+  return template.map(([categoryNameUkr, subcategories]) => {
+    return serializeNonPOJOs(new Category(categoryNameUkr, subcategories)) as object;
+  });
+};
+
+export { mapTemplateToSerializedCategory };
